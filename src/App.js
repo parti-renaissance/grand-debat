87063e63d19c00d72fc435fdc5e344fad6890428
Charles d'Oiron <doironcharles@gmail.com>
2019-01-11 18:15:01 +0100

    contre-naturan2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ContreNaturan from './js/components/bonjour.js';
 class App extends Component {
