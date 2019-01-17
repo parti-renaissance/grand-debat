e589d615a7be9804b7b4fb3d2dacf378139f8f72
Marine Boudeau <m14868@gmail.com>
2019-01-17 21:46:04 +0100

    Add a few additional tweaks before release 🦈

--- src/js/components/Cta.js
+++ src/js/components/Cta.js
@@ -7,7 +7,7 @@ const Cta = props => (
 				<div>
 					<h4>Notre révolution démocratique à besoin de vous</h4>
 					<p>
-						L'indépendance d'EM!, c'est aussi son indépendance financière. Grâce aux dons, et uniquement grâce à
+						L'indépendance de LaREM, c'est aussi son indépendance financière. Grâce aux dons, et uniquement grâce à
 						eux, nous avons réussi à lancer un mouvement inédit. Cette liberté, c'est notre ADN. Contribuez.
 					</p>
 				</div>
@@ -31,15 +31,6 @@ const Cta = props => (
 					</div>
 				</div>
 			</div>
-		</div>
-		<div className="footer">
-			<ul>
-				<li><a href="https://en-marche.fr/mentions-legales" target="_blank" rel="noopener noreferrer">Mentions Légales</a></li>
-				<li><a href="https://en-marche.fr/politique-cookies" target="_blank" rel="noopener noreferrer">Politique de Cookies</a></li>
-				<li><a href="https://en-marche.fr/politique-protection-donnees" target="_blank" rel="noopener noreferrer">Politique de Protection des Données</a></li>
-				<li><a href="https://github.com/EnMarche/grand-debat" target="_blank" rel="noopener noreferrer">Code sur Github</a></li>
-			</ul>
-			<span className="muted">© La République En Marche !</span>
 		</div>
 </div>
 );
